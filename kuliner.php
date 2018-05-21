5a35c9beb1d0e69f5d3b01b5eaf510eb570499c4
sinthaistikomah <39285031+sinthaistikomah@users.noreply.github.com>
2018-05-22 06:49:45 +0700

    Add files via upload

---
+++ kuliner.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<!--untuk mengaktifkan responsive design-->
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<title>Dream Land</title>
+	<!--panggil file css bootstrapp-->
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<style type="text/css">
+  .img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
+
+<body>
+
+	<div class="jumbotron text-center" style="margin-bottom:0">
+  		<img src="logo.png" width="300px">
+	</div>
+
+	<nav class="navbar navbar-expand-sm bg-info navbar-dark">
+  	<ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Beranda</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Lifestyle</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Kesehatan</a>
+    </li>
+    <li class="nav-item active">
+    	<a class="nav-link" href="#">Kuliner</a>
+    </li>
+    <li class="nav-item">
+    	<a class="nav-link" href="#">Profil</a>
+    </li>
+    <li class="nav-item">
+    	<a class="nav-link" href="#">Buat Artikel</a>
+    </li>
+    <li class="nav-item">
+    	<a class="nav-link" href="#">Logout</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="row">
+    <div class="col-md-8">
+      <div class="img">
+      <form>
+      <img src="img/tahu.jpg" class="img img-thumbnail">
+      <h1 align="center">Resep Praktis Tahu Tauge Enak & Sehat Menggugah Selera</h1>
+      <p class="text-justify" style="text-align: center;">
+        <b>DreamLand.com</b>Tahu dan kecambah merupakan bahan masakan yang memiliki nutrisi super sehat buat tubuh. Dua bahan masakan ini juga mudah ditemukan di pasar dan harganya pun terbilang murah. Menariknya, dari dua bahan ini akan tercipta masakan super enak dan sehat menggugah selera. Dimasak tumis dan diberi sedikit kecap, dijamin deh nafsu makan akan semakin meningkat dengan menu satu ini. 
+
+      <p>Lalu, bagaimana resep tahu tauge atau tahu kecambah yang enak dan sehat menggugah selera? Simak resepnya di bawah ini. 
+
+        <ul>Bahan
+
+<li>100 gram tauge kacang hijau</li>
+<li>1 kotak tahu (potong dadu, goreng hingga bagian luarnya kecokelatan)</li>
+<li>5 siung bawang putih (iris tipis)</li>
+<li>2 siung bawang merah (iris tipis)</li>
+<li>2 buah cabai merah besar (buang isi, iris serong)</li>
+<li>3 buah cabai rawit (iris serong)</li>
+<li>Lada bubuk (secukupnya)</li>
+<li>Garam (secukupnya)</li>
+<li>Gula (secukupnya)</li>
+<li>Kaldu ayam bubuk (secukupnya)</li>
+<li>Kecap manis (secukupnya)</li>
+<li>Minyak</li></ul>
+</p>
+Cara Membuat
+
+<p>Tumis bawang putih, bawang merah, cabai merah dan cabai rawit hingga harum.
+Masukkan tauge ke dalam tumisan bumbu. 
+Tambahkan lada bubuk, garam dan gula secukupnya. 
+Masukkan tahu kemudian aduk rata, masak hingga tauge matang.
+Tambahkan kaldu ayam bubuk dan kecap manis secukupnya, aduk-aduk semua bahan, koreksi rasa kemudian matangkan. 
+Sajikan tahu tauge bersama nasi putih hangat dan taburan bawang merah goreng.
+Resep yang sangat mudah bukan? Selamat mencoba resep praktis tahu tauge enak dan sehat menggugah selera ini. Semoga keluarga di rumah suka. 
+</p>
+</p>
+ </form>
+ </div>
+<hr>
+
+<!--komentar-->
+<section class="comment" id="comment">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h2>Komentar</h2>
+        <hr>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="row">
+  <div class="col-sm-8 col-sm-offset-2">
+   <form>
+     <div class="form-group">
+       <label for="nama">Nama</label>
+       <input type="text" id="nama" class="form-control" placeholder="masukkan nama">
+     </div>
+      <div class="form-group">
+       <label for="email">Email</label>
+       <input type="email" id="email" class="form-control" placeholder="masukkan email">
+     </div>
+     <div class="form-group">
+       <label for="pesan">Pesan</label>
+      <textarea class="form-control" rows="10" placeholder="masukkan pesan anda"></textarea>
+    </div>
+    <button type="submit" class=" btn btn-primary">Kirim</button>
+    <!--akhir komentar-->
+
+    <!--footer-->
+    <footer>
+      <div class="container text-center">
+        <div class="row">
+        <div class="col-sm-12">
+        <p>&copy; copyright 2018| Dream Land </p> 
+
+        </div>
+      </div>
+    </footer>
+
+    
+   </form> 
+  </div>
+</div>
+
+
+</body>
+</html>
